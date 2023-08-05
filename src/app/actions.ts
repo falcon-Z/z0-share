7aada33bb25dec6afd29d44d1317029cb0d19006
sai madhan <saimadhan@outlook.in>
2023-08-05 15:07:51 +0530

    handles user registration

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,7 @@
 import { cookies } from "next/dist/client/components/headers";
 import { redirect } from "next/navigation";
 
-export default async function handleLogin(data: FormData) {
+export async function handleLogin(data: FormData) {
   const email = data.get("email");
   const password = data.get("password");
 
@@ -16,6 +16,31 @@ export default async function handleLogin(data: FormData) {
   });
 
   const authData = await response.json();
+  if (authData.token) {
+    cookies().set("token", authData.token);
+    redirect("/");
+  } else {
+    redirect(`/auth/signin/?error=true&message=${authData.message}`);
+  }
+}
+
+export async function handleRegistration(data: FormData) {
+  const name = data.get("name");
+  const email = data.get("email");
+  const password = data.get("password");
+
+  const response = await fetch(`${process.env.API_HOST_URI}/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, password }),
+  });
+
+  const authData = await response.json();
+
+  console.log(authData);
+
   if (authData.token) {
     cookies().set("token", authData.token);
     redirect("/");
--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -2,6 +2,7 @@
 
 import SignInForm from "@falcon-z/app/_components/auth/signInForm";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import Link from "next/link";
 
 export default function SignIn() {
   return (
@@ -11,6 +12,18 @@ export default function SignIn() {
         SignIn
       </h2>
       <SignInForm />
+
+      <div>
+        <p className="text-gray-400 text-base">
+          Dont have an account yet?{" "}
+          <Link
+            href={"/auth/signup"}
+            className="text-gray-200 hover:underline font-bold"
+          >
+            Create Account
+          </Link>{" "}
+        </p>
+      </div>
     </>
   );
 }
